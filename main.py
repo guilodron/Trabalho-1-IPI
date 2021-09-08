805fce0daaffb37a3a4d63da599444acc768479c
Guilherme Pires <guilodron7@gmail.com>
2021-09-08 13:08:09 -0300

    Implemented 2 assignment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 # \brief Checa se a imagem está em escala de cinza
 # 
@@ -53,13 +54,132 @@ def dec_int(original_image):
     interpolada = interpolate(reduzida)
     return interpolada
 
-original = cv2.imread('images/test80.jpg')
-compressed = cv2.resize(original, (int(original.shape[1]/2), int(original.shape[0]/2)), interpolation = cv2.INTER_CUBIC)
-interpolated = cv2.resize(compressed, (int(compressed.shape[1]*2), int(compressed.shape[0]*2)), interpolation = cv2.INTER_CUBIC)
+# \brief Aplica um filtro de aguçamento Laplaciano na imagem
+# 
+# \param image imagem a ser filtrada
+# \return matriz representando a imagem filtrada
+def edge_improv(image):
+    kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
 
-my_interpolation = dec_int(original)
-cv2.imshow('Original', original)
-cv2.imshow('Resized opencv', interpolated)
-cv2.imshow('My resized', my_interpolation)
+    filtered = cv2.filter2D(image, ddepth=-1, kernel=kernel)
+    improved = cv2.add(image, filtered)
+    return improved
 
-cv2.waitKey()
+# \brief Aplica a transformação de potência em uma imagem
+# 
+# \param image Imagem a ser transformada
+# \param gamma expoente a ser aplicado na transformaçao
+# \return Matriz com os valores transformados
+def power_law(image, gamma):
+    cv2.imshow('Gamma: ' + str(gamma) ,np.array(np.uint8(255*(image/255)**gamma)))
+
+# \brief Realiza a equalizaçao de histograma a fim de balancear a imagem
+# 
+# \param image Imagem a ser corrigida
+# \return matriz com os valores corrigidos
+def equalize_hist(image):
+    if not(isGray(image)):
+        r, g, b = cv2.split(image)
+        equalized_r = cv2.equalizeHist(r)
+        equalized_g = cv2.equalizeHist(g)
+        equalized_b = cv2.equalizeHist(b)
+        return cv2.merge([equalized_r, equalized_g, equalized_b])
+    else:
+        return cv2.equalizeHist(image)
+
+def Prog1():
+    # manual resize
+    original_image = cv2.imread('images/test80.jpg')
+    my_resized = dec_int(original_image)
+    # opencv resize
+    compressed = cv2.resize(original_image, (int(original_image.shape[1]/2), int(original_image.shape[0]/2)), interpolation = cv2.INTER_CUBIC)
+    interpolated = cv2.resize(compressed, (int(compressed.shape[1]*2), int(compressed.shape[0]*2)), interpolation = cv2.INTER_CUBIC)
+    # cv2.imshow('My resize', my_resized)
+    # cv2.imshow('OpenCV resize', interpolated)
+    my_resized_improved = edge_improv(my_resized)
+    opencv_resized_improved = edge_improv(interpolated)
+    cv2.imshow('My resize improved', my_resized_improved)
+    cv2.imshow('OpenCV resize improved', opencv_resized_improved)
+
+def Prog2():
+    # car.png manipulations
+    car = cv2.imread('images/car.png')
+    cv2.imshow('Car Original', car)
+    power_law(car, 0.3)
+    power_law(car, 0.6)
+    power_law(car, 0.9)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    power_law(car, 2)
+    power_law(car, 3)
+    power_law(car, 4)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # crowd.png manipulations
+    crowd = cv2.imread('images/crowd.png')
+    cv2.imshow('Crowd Original', crowd)
+    power_law(crowd, 0.3)
+    power_law(crowd, 0.6)
+    power_law(crowd, 0.9)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    power_law(crowd, 2)
+    power_law(crowd, 3)
+    power_law(crowd, 4)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # university.png manipulations
+    university = cv2.imread('images/university.png')
+    cv2.imshow('Crowd Original', university)
+    power_law(university, 0.3)
+    power_law(university, 0.6)
+    power_law(university, 0.9)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    power_law(university, 2)
+    power_law(university, 3)
+    power_law(university, 4)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # Equalizando histograma da imagem car.png
+    equalized_car = equalize_hist(car)
+    equalized_crowd = equalize_hist(crowd)
+    equalized_university = equalize_hist(university)
+    cv2.imshow('Equalized car', equalized_car)
+    cv2.imshow('Equalized crowd', equalized_crowd)
+    cv2.imshow('Equalized university', equalized_university)
+    cv2.waitKey()   
+    cv2.destroyAllWindows()
+
+    # Preparando e plotando histogramas e funções de distribuição acumulada
+    fig, ax = plt.subplots(2,2)
+    plt.tight_layout()
+    fig.set_size_inches(10,7)
+    grayscale_car = cv2.cvtColor(car, cv2.COLOR_BGR2GRAY)
+    original_flat = grayscale_car.flatten()
+    ax[0][0].hist(original_flat, bins=255, range=[0,255])
+    ax[0][0].set_title('Histograma não equalizado da imagem car.png')
+    count, number = np.histogram(original_flat, bins=255)
+    ax[0][1].plot(np.cumsum(count/len(original_flat)))
+    ax[0][1].set_title('CDF da imagem não equalizada')
+    grayscale_car_equalized = cv2.cvtColor(equalized_car, cv2.COLOR_BGR2GRAY)
+    flat = grayscale_car_equalized.flatten()
+    ax[1][0].hist(flat, bins=255)
+    ax[1][0].set_title('Histograma equalizado da imagem car.png')
+    count, number = np.histogram(flat, bins=255)
+    ax[1][1].plot(np.cumsum(count/len(flat)))
+    ax[1][1].set_title('CDF da imagem equalizada')
+    plt.show()
+
+# Prog1()
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+# Prog2()
+# cv2.waitKey()
+
+
+print('ok')
